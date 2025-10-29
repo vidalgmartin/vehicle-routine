fcb7be5e358b961a065977a07135e0b054a7e11f
vidalgmartin <matchacow@hotmail.com>
2025-10-29 15:32:12 -0400

    removed boilerplate

---
+++ server/Controllers/TestingController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace server.Controllers;
+
+[ApiController]
+[Route("/api")]
+public class TestingController : ControllerBase
+{
+    [HttpGet("test")]
+    public IActionResult GetTest()
+    {
+        return Ok("Helloge world!");
+    }
+}
--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+852064c3a76f1ecbe7a43849ee2525b31c0e352e")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
