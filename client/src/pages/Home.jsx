da7df1809d365ff0e7f513ea81bf33c677f8cc12
vidalgmartin <matchacow@hotmail.com>
2025-11-07 09:07:36 -0500

    r3f and home page update

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,19 +1,40 @@
 import { Link } from "react-router-dom"
-import './home.css'
+import { Canvas, useFrame } from "@react-three/fiber"
+import { useRef } from "react"
+
+function Donut() {
+    const ref = useRef()
+
+    useFrame(() => {
+        ref.current.rotation.z += 0.0005;
+    })
+
+    return (
+        <mesh ref={ref} position={[0, 0, -15]} rotation={[2, 0.4, 0]}>
+            <torusGeometry args={[10, 3, 16, 36]}/>
+            <meshStandardMaterial wireframe color="white" />
+        </mesh>
+    )
+}
 
 export default function Home() {
-    
     return (
         <div className="home-container">
 
             <div className="home-content-container">
+                <div className="home-content" id="donut-container">
+                    <Canvas camera={{ position: [-0.4, 1, 6] }}>
+                        <ambientLight />
+                        <Donut />
+                    </Canvas>
+                </div>
+
                 <div className="home-content home-section">
                     <div className="home-text">
-                        <h2>Store, share and view</h2>
-                        <h2>3D art</h2>
+                        <h2>Vehicle Maintenance</h2>
+                        <h2>Reminder</h2>
                     </div>
                             
-                    
                     <div className="home-btns">
                         <Link to="/signup">
                             <button>
