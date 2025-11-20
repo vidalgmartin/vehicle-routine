c0d17b502901adff810d3f3e55aa632bea2dd158
vidalgmartin <matchacow@hotmail.com>
2025-11-20 14:44:33 -0500

    User model

---
+++ server/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace server.Models;
+
+public class User
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string Username { get; set; } = string.Empty;
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+}
