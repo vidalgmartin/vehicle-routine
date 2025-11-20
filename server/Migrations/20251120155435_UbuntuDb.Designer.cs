7f8ed059574dcacfad8f0efc8cf6304121746f24
Martin <vidalgmartin@hotmail.com>
2025-11-20 11:12:13 -0500

    Downgraded .NET version and created migration for Ubuntu env

--- server/Migrations/20251120155435_UbuntuDb.Designer.cs
+++ server/Migrations/20251120155435_UbuntuDb.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using server.Database;
 
@@ -10,9 +11,11 @@ using server.Database;
 namespace server.Migrations
 {
     [DbContext(typeof(ServerDbContext))]
-    partial class ServerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251120155435_UbuntuDb")]
+    partial class UbuntuDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
